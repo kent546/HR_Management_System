6f11f35aaa917efd5b19185e090eda6618ab8a63
kent546 <kentargate123@gmail.com>
2024-10-19 01:18:56 +0800

    hello

---
+++ db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('localhost','root','','hrms')or die("Could not connect to mysql".mysqli_error($con));
---
+++ home.php
@@ -0,0 +1,14 @@
+<?php include 'db_connect.php' ?>
+<style>
+   
+</style>
+
+<div class="containe-fluid">
+
+
+	
+
+</div>
+<script>
+	
+</script>
